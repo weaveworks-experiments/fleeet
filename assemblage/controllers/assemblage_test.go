32157620a7713ae090b557110a3e801c0ea7784d
Michael Bridgen <mikeb@squaremobius.net>
2021-02-26 17:39:07 +0000

    Test that a minimal Assemblage can be created
    Signed-off-by: Michael Bridgen <mikeb@squaremobius.net>

--- assemblage/controllers/assemblage_test.go
+++ assemblage/controllers/assemblage_test.go
@@ -6,12 +6,14 @@ package controllers
 
 import (
 	"context"
+	"time"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"k8s.io/client-go/kubernetes/scheme"
 	ctrl "sigs.k8s.io/controller-runtime"
-	//	fleetv1alpha1 "github.com/squaremo/fleeet/assemblage/api/v1alpha1"
+
+	fleetv1 "github.com/squaremo/fleeet/assemblage/api/v1alpha1"
 )
 
 var _ = Describe("assemblage controller", func() {
@@ -41,5 +43,20 @@ var _ = Describe("assemblage controller", func() {
 
 	AfterEach(func() {
 		stopManager()
+	})
+
+	It("can be created", func() {
+		asm := fleetv1.Assemblage{
+			Spec: fleetv1.AssemblageSpec{
+				Syncs: []fleetv1.Sync{},
+			},
+		}
+		asm.Name = "asm"
+		asm.Namespace = "default"
+
+		ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+		defer cancel()
+
+		Expect(k8sClient.Create(ctx, &asm)).To(Succeed())
 	})
 })
