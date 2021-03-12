9f60a9a5bd63d49ca3d8a3f72cdfa8593b0c2718
Michael Bridgen <mikeb@squaremobius.net>
2021-03-12 16:31:27 +0000

    Test that the controller creates a git source
    The idea of an assemblage is that it will create all the sources and
    kustomizations implied by the definition. This adds a test which
    checks the very least it should do, which is to create a source.
    
    The test fails, at this point -- the controller is yet to be written.
    
    Signed-off-by: Michael Bridgen <mikeb@squaremobius.net>

--- assemblage/controllers/assemblage_test.go
+++ assemblage/controllers/assemblage_test.go
@@ -6,19 +6,34 @@ package controllers
 
 import (
 	"context"
+	"math/rand"
 	"time"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes/scheme"
 	ctrl "sigs.k8s.io/controller-runtime"
 
+	sourcev1 "github.com/fluxcd/source-controller/api/v1beta1"
 	fleetv1 "github.com/squaremo/fleeet/assemblage/api/v1alpha1"
 )
+
+func randomStr(prefix string) string {
+	const randomStrCount = 5
+	var letterRunes []rune = []rune("abcdefghijklmnopqrstuvwxyz1234567890")
+	b := make([]rune, randomStrCount)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return prefix + string(b)
+}
 
 var _ = Describe("assemblage controller", func() {
 	var manager ctrl.Manager
 	var stopManager func()
+	var namespace *corev1.Namespace
 
 	BeforeEach(func() {
 		var err error
@@ -30,12 +45,18 @@ var _ = Describe("assemblage controller", func() {
 		assemblageReconciler := &AssemblageReconciler{
 			Client: manager.GetClient(),
 			Scheme: scheme.Scheme,
+			Log:    ctrl.Log.WithName("controllers").WithName("Assemblage"),
 		}
 		Expect(assemblageReconciler.SetupWithManager(manager)).To(Succeed())
+
+		namespace = &corev1.Namespace{}
+		namespace.Name = randomStr("test-ns-")
+		Expect(k8sClient.Create(context.Background(), namespace)).To(Succeed())
 
 		var ctx context.Context
 		ctx, stopManager = context.WithCancel(context.Background())
 		go func() {
+			defer GinkgoRecover()
 			err = manager.Start(ctx)
 			Expect(err).ToNot(HaveOccurred())
 		}()
@@ -43,6 +64,7 @@ var _ = Describe("assemblage controller", func() {
 
 	AfterEach(func() {
 		stopManager()
+		Expect(k8sClient.Delete(context.Background(), namespace)).To(Succeed())
 	})
 
 	It("can be created", func() {
@@ -52,11 +74,55 @@ var _ = Describe("assemblage controller", func() {
 			},
 		}
 		asm.Name = "asm"
-		asm.Namespace = "default"
+		asm.Namespace = namespace.Name
 
 		ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
 		defer cancel()
 
 		Expect(k8sClient.Create(ctx, &asm)).To(Succeed())
+	})
+
+	It("creates GOTK objects", func() {
+		asm := fleetv1.Assemblage{
+			Spec: fleetv1.AssemblageSpec{
+				Syncs: []fleetv1.Sync{
+					{
+						Source: fleetv1.SourceSpec{
+							Git: &fleetv1.GitSource{
+								URL: "https://github.com/cuttlefacts-app",
+								Version: fleetv1.GitVersion{
+									Revision: "bd6ef78",
+								},
+							},
+						},
+						Package: &fleetv1.PackageSpec{
+							Kustomize: &fleetv1.KustomizeSpec{
+								Path: "deploy",
+							},
+						},
+					},
+				},
+			},
+		}
+		asm.Name = randomStr("asm")
+		asm.Namespace = namespace.Name
+
+		ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+		defer cancel()
+		Expect(k8sClient.Create(ctx, &asm)).To(Succeed())
+
+		// eventually we should see a git repository source and
+		// kustomization created in the same namespace.
+		expectedGitName := types.NamespacedName{
+			Name:      "asm-source-1", // FIXME decide what this is supposed to be
+			Namespace: asm.Namespace,
+		}
+		Eventually(func() bool {
+			var source sourcev1.GitRepository
+			if err := k8sClient.Get(context.Background(), expectedGitName, &source); err != nil {
+				return false
+			}
+			return source.Name == expectedGitName.Name
+		}, "5s", "1s").Should(BeTrue())
 	})
 })
